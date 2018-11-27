c1da2e0d2e46d6933af643f81e84f34e7c9555e2
Angela Kwon <kwonangela7@berkeley.edu>
2018-11-27 15:02:55 -0800

    extracting parameters from form

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -3,19 +3,24 @@ class PlaylistController < ActionController::Base
 
 	def parse
 		@playlist = Playlist.create(user_params) 
+
+
 		# Assuming the user typed in the name/selected a seed track (input: name), limited to one as of now
 
-		@track = RSpotify::Base.search(@playlist[:track], 'track', limit: 1) # using song keywords to get song id
-		@t_id = @track.first.id # since the call above returns an array of (presumably) 1 track artist
+		# @track = RSpotify::Base.search(@playlist[:track], 'track', limit: 1) # using song keywords to get song id
+		# @t_id = @track.first.id # since the call above returns an array of (presumably) 1 track artist
 
-		# do the same thing to get the artist id
-		@artist = RSpotify::Base.search(@playlist[:artist], 'artist', limit: 1)
-		@a_id = @artist.first.id
+		# # do the same thing to get the artist id
+		# @artist = RSpotify::Base.search(@playlist[:artist], 'artist', limit: 1)
+		# @a_id = @artist.first.id
 
 		# try to valiate that id's are strings, though they naturally should be
-		@playlist.seed_tracks = [@t_id] 
-		@playlist.seed_artists = [@a_id]
-		@playlist.seed_genres = [@playlist[:genre]] #@R - easiest way to do this would be a drop down, so I'm going to assume drop down	
+		# @playlist.seed_tracks = [@t_id] 
+		# @playlist.seed_artists = [@a_id]
+		# @playlist.seed_genres = [@playlist[:genre]] #@R - easiest way to do this would be a drop down, so I'm going to assume drop down	
+	
+		# acousticness, danceability (min and max), energy, instrumentalness, liveliness, loudness
+
 	end
 
 	def user_params
@@ -24,7 +29,8 @@ class PlaylistController < ActionController::Base
 	end
 
 	def generate
-		# 
-		recommendations = RSpotify::Recommendations.generate(seed_tracks: @playlist.seed_tracks, seed_genres: @playlist.seed_genres, seed_artists: @playlist.seed_artists, target_energy: 1.0)
+		recommendations = RSpotify::Recommendations.generate(max_acousticness:, max_danceability:, max_energy:, max_instrumentalness:, target_loudness:) 
+)
+		#recommendations = RSpotify::Recommendations.generate(seed_tracks: @playlist.seed_tracks, seed_genres: @playlist.seed_genres, seed_artists: @playlist.seed_artists) #target_energy: 1.0)
 	end
 end
