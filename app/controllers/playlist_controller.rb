1c00e2bfc12284dac7b42dcaee027eeb59371286
Matthew Jeng <mjeng@berkeley.edu>
2018-11-29 15:07:46 -0800

    super janky finish xd

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -1,7 +1,9 @@
 class PlaylistController < ActionController::Base
 
-	def collect 
+	def collect
 		#collects user preferences
+		@hash = params.to_unsafe_h[:hash]
+		puts "IN COLLECT HASH IS #{@hash}"
 		render 'collect'
 	end
 
@@ -18,10 +20,10 @@ class PlaylistController < ActionController::Base
 		@a_id = @artist.first.id
 
 		#try to valiate that id's are strings, though they naturally should be
-		@playlist.seed_tracks = [@t_id] 
+		@playlist.seed_tracks = [@t_id]
 		@playlist.seed_artists = [@a_id]
-		@playlist.seed_genres = [@playlist[:genre]] 
-	
+		@playlist.seed_genres = [@playlist[:genre]]
+
 		# @max_acousticness = params[:acousticness] # form submission format might be different
 		# @max_danceability = params[:danceability]
 		# @max_energy = params[:energy]
@@ -30,7 +32,7 @@ class PlaylistController < ActionController::Base
 		# @max_loudness = params[:loudness]
 
 		@max_acousticness = 0.5 # form submission format might be different
-		@max_danceability = 0.5 
+		@max_danceability = 0.5
 		@max_energy = 0.5
 		@max_instrumentalness = 0.7
 		@max_liveness = 0.2
@@ -41,9 +43,10 @@ class PlaylistController < ActionController::Base
 
 
 	def generate
+		@hash = params[:hash]
 		@spotify_user = RSpotify::User.new(@hash)
-   
-	    @seeds = {track: "Delicate", artist: "Taylor Swift", genre: "rock"}
+
+    @seeds = {track: "Delicate", artist: "Taylor Swift", genre: "rock"}
 		@track = RSpotify::Base.search(@seeds[:track], 'track', limit: 1) # using song keywords to get song id
 		@t_id = @track.first.id # since the call above returns an array of (presumably) 1 track artist
 
@@ -51,19 +54,20 @@ class PlaylistController < ActionController::Base
 		@artist = RSpotify::Base.search(@seeds[:artist], 'artist', limit: 1)
 		@a_id = @artist.first.id
 
-	    @max_acousticness = 1.0 # form submission format might be different
-	    @max_danceability = 1.0
-	    @max_energy = 1.0
-	    @max_instrumentalness = 1.0
-	    @max_liveness = 1.0
+    @max_acousticness = 1.0 # form submission format might be different
+    @max_danceability = 1.0
+    @max_energy = 1.0
+    @max_instrumentalness = 1.0
+    @max_liveness = 1.0
 
-	    #recommendations = RSpotify::Recommendations.generate(seed_artists: [@t_id], seed_tracks: [@a_id], max_acousticness: @max_acousticness, max_danceability: @max_danceability, max_energy: @max_energy, max_instrumentalness: @max_instrumentalness, max_liveness: @max_liveness) #target_loudness: @max_loudness
-	    recommendations = RSpotify::Recommendations.generate(seed_artists: ['4NHQUGzhtTLFvgF5SZesLK'], seed_tracks: ['0c6xIDDpzE81m2q797ordA'], max_acousticness: @max_acousticness, max_danceability: @max_danceability, max_energy: @max_energy, max_instrumentalness: @max_instrumentalness, max_liveness: @max_liveness) #target_loudness: @max_loudness) 
+    #recommendations = RSpotify::Recommendations.generate(seed_artists: [@t_id], seed_tracks: [@a_id], max_acousticness: @max_acousticness, max_danceability: @max_danceability, max_energy: @max_energy, max_instrumentalness: @max_instrumentalness, max_liveness: @max_liveness) #target_loudness: @max_loudness
+    recommendations = RSpotify::Recommendations.generate(seed_artists: ['4NHQUGzhtTLFvgF5SZesLK'], seed_tracks: ['0c6xIDDpzE81m2q797ordA'], max_acousticness: @max_acousticness, max_danceability: @max_danceability, max_energy: @max_energy, max_instrumentalness: @max_instrumentalness, max_liveness: @max_liveness) #target_loudness: @max_loudness)
 
 
-	    @new_playlist = @spotify_user.create_playlist!('Recommendations 1')
-	    #puts "PLAYLIST: #{playlist}"
-	    @new_playlist.add_tracks!(recommendations.tracks)
+    @new_playlist = @spotify_user.create_playlist!('Recommendations 1')
+		puts "NEW PLAYLIST !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! #{@new_playlist}"
+    #puts "PLAYLIST: #{playlist}"
+    @new_playlist.add_tracks!(recommendations.tracks)
 
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def spotify
     @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
     @hash = @spotify_user.to_hash
-    puts @hash
+    puts "USER#SPOTIFY HASH IS #{@hash}"
     @country = @spotify_user.country #=> "US"
     puts "COUNTRY: #{@country}"
     @email = @spotify_user.email   #=> "example@email.com"
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.create(token: @hash, email: @email)
     @playlists = @spotify_user.playlists(limit: 5)
 
-    
+
     # playlist = spotify_user.create_playlist!('zzzzzzzzzzzzzzzzzzzzzz')
     # puts "PLAYLIST: #{playlist}"
     # # Add tracks to a playlist in user's Spotify account
